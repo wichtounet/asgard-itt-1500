6470c6c9302b0b5b91f6275c7c848c0cdacd8dbc
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-27 16:02:48 +0200

    Prepare Jenkins support

---
+++ JenkinsFile.groovy
@@ -0,0 +1,13 @@
+node {
+   stage 'git'
+   checkout scm
+   sh 'git submodule update --init"
+
+   stage 'pre-analysis'
+   sh 'cppcheck --xml-version=2 --enable=all --std=c++11 src/*.cpp 2> cppcheck_report.xml'
+   sh 'sloccount --duplicates --wide --details src > sloccount.sc'
+   sh 'cccc src/*.cpp || true'
+
+   stage 'sonar'
+   sh '/opt/sonar-runner/bin/sonar-runner'
+}
